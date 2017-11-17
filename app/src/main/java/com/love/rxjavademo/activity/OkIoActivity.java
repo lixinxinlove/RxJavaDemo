3ac4f78df58858f1dbf9d093c99864982492e5c3
lixinxin <895330766@qq.com>
2017-11-17 15:52:07 +0800

    添加 okHttp utils

--- app/src/main/java/com/love/rxjavademo/activity/OkIoActivity.java
+++ app/src/main/java/com/love/rxjavademo/activity/OkIoActivity.java
@@ -2,15 +2,17 @@ package com.love.rxjavademo.activity;
 
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.SystemClock;
 import android.view.View;
 import android.widget.Toast;
 
 import com.love.rxjavademo.R;
 import com.love.rxjavademo.util.OkHttpUtils;
+import com.love.rxjavademo.util.Time2Utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -23,7 +25,6 @@ public class OkIoActivity extends BaseActivity {
 
     private String imageUrl = "http://7xi8d6.com1.z0.glb.clouddn.com/20171025112955_lmesMu_katyteiko_25_10_2017_11_29_43_270.jpeg";
 
-
     @Override
     protected int getContentViewId() {
         return R.layout.activity_ok_io;
@@ -32,6 +33,7 @@ public class OkIoActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
@@ -47,7 +49,15 @@ public class OkIoActivity extends BaseActivity {
 
 
     public void okHttp(View view) {
-        OkHttpUtils.getInstance().get(imageUrl, callback);
+      //  OkHttpUtils.getInstance().get(imageUrl, callback);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "lixinxin");
+        map.put("key1", "lixinxin");
+        map.put("key2", "lixinxin");
+        map.put("key3", "lixinxin");
+
+        OkHttpUtils.getInstance().post(imageUrl, map, callback);
     }
 
     private Callback callback = new Callback() {
@@ -58,7 +68,7 @@ public class OkIoActivity extends BaseActivity {
 
         @Override
         public void onResponse(Call call, Response response) throws IOException {
-            String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + SystemClock.currentThreadTimeMillis() + "lee.jpeg";
+            String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + Time2Utils.getToday() + "lee.jpeg";
             File file = new File(filePath);
             BufferedSink sink = Okio.buffer(Okio.sink(file));
             Source source = response.body().source();
@@ -77,6 +87,7 @@ public class OkIoActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        //取消没有完成的请求
         OkHttpUtils.getInstance().cancel();
     }
 }
--- app/src/main/java/com/love/rxjavademo/util/OkHttpUtils.java
+++ app/src/main/java/com/love/rxjavademo/util/OkHttpUtils.java
@@ -1,11 +1,18 @@
 package com.love.rxjavademo.util;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by android on 2017/11/17.
@@ -17,10 +24,24 @@ public class OkHttpUtils {
 
     private static OkHttpClient okHttpClient;
 
+    private static Interceptor interceptor;
+
     private Call call;
 
     public static synchronized OkHttpUtils getInstance() {
         if (instance == null) {
+            interceptor = new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request request = chain.request();
+                    Log.e("lee", "请求前");
+                    Response response = chain.proceed(request);
+                    //---------请求之后------------
+                    Log.e("lee", "请求后");
+                    return response;
+                }
+            };
+
             instance = new OkHttpUtils();
         }
         return instance;
@@ -31,6 +52,7 @@ public class OkHttpUtils {
         okHttpClient = new OkHttpClient.Builder()
                 .readTimeout(10, TimeUnit.SECONDS)
                 .writeTimeout(10, TimeUnit.SECONDS)
+                .addInterceptor(interceptor)
                 .build();
     }
 
@@ -39,6 +61,23 @@ public class OkHttpUtils {
         call = okHttpClient.newCall(request);
         call.enqueue(callback);
     }
+
+
+    public void post(String url, Map<String, String> map, Callback callback) {
+        FormBody.Builder builder = new FormBody.Builder();
+
+        for (String key : map.keySet()) {
+            builder.add(key, map.get(key));
+            Log.e("lee",key+"--"+map.get(key));
+        }
+
+        FormBody body = builder.build();
+
+        Request request = new Request.Builder().url(url).post(body).build();
+        call = okHttpClient.newCall(request);
+        call.enqueue(callback);
+    }
+
 
     public void cancel() {
         if (call != null) {
