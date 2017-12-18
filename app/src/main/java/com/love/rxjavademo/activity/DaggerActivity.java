637b901b6a19e5058ef6b80677243b24f7b2b7ac
lixinxin <895330766@qq.com>
2017-12-18 10:56:06 +0800

    添加  Dialog

--- app/src/main/java/com/love/rxjavademo/activity/DaggerActivity.java
+++ app/src/main/java/com/love/rxjavademo/activity/DaggerActivity.java
@@ -13,29 +13,19 @@ import com.love.rxjavademo.dagger.module.UserEntityModule;
 import javax.inject.Inject;
 
 public class DaggerActivity extends BaseActivity {
-
     @Inject
     UserEntity userEntity;
-
-
     @Inject
     UserEntity userEntity1;
-
-
     @Inject
     LeeDialog leeDialog;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DaggerUserEntityComponent.builder().leeDialogModule(new LeeDialogModule(this)).userEntityModule(new UserEntityModule()).build().lixinxin(this);
-
-
         userEntity.setUserName("lee");
-
         leeDialog.show();
-
         Log.e("DaggerActivity", userEntity.toString());
         Log.e("DaggerActivity", userEntity1.toString());
     }
